d2e381fa51ac58f5a194857b316388b50c3079d4
Ramon-Alexandru Salagean <ramon-alexandru.salagean@cloudflight.io>
2024-02-05 20:12:55 +0100

    Add HUD and create globals file

---
+++ scripts/globals.gd
@@ -0,0 +1,11 @@
+class_name Globals
+
+const CHARACTER_START_POSITION := Vector2i(150, 486)
+const CAMERA_START_POSITION := Vector2i(576, 324)
+
+const SCORE_MODIFIER: int = 10
+const SPEED_MODIFIER: int = 5000
+
+const START_SPEED: float = 10.0
+const MAX_SPEED: int = 25
+const MAX_DIFFICULTY: int = 3
